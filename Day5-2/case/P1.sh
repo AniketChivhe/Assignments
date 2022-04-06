a6054743d6fb21e3941532dcf240933362f4abcb
AniketChivhe <aniketchivhe@gmail.com>
2022-04-06 23:02:42 +0530

    [Aniket]case P1 is solved

---
+++ Day5-2/case/P1.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -x
+
+read -p "enter single digit no." SingleDigit
+
+case $SingleDigit in
+	1)
+	echo "one";;
+	2)
+	echo "two";;
+	3)
+	echo "three";;
+	4)
+	echo "four";;
+	5)
+	echo "five";;
+	6)
+	echo "six";;
+	7)
+	echo "seven";;
+	8)
+	echo "eight";;
+	9)
+	echo "nine";;
+	0)
+	echo "zero";;
+	*)
+	echo "Please enter single digit no."
+esac
